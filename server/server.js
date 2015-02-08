b597e345bc0299664e629a8a4a20b6404c25b401
arvindh5686 <aravindh@Aravindhs-MacBook-Air.local>
2015-02-08 00:59:29 -0800

    second commit

--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@
  */
 
 var express     = require( 'express' ),
-    cons        = require( 'consolidate' ),
     http        = require( 'http' ),
     path        = require( 'path' ),
     io          = require( 'socket.io' ),
@@ -18,36 +17,17 @@ var server = exports.server = express();
 server.configure( function() {
     server.set( 'port', process.env.PORT || appConfig.server.port );
     server.set( 'views', path.join( __dirname, './../app' ) );
-    server.engine( 'html', cons.hogan );
-    server.set( 'view engine', 'html' );
-    server.engine( 'hjs', cons.hogan );
-    server.set( 'view engine', 'hjs' );
-
+    
     server.use( express.bodyParser() );
     server.use( express.methodOverride() );
     server.use( express.static( path.join( __dirname, './../app' ) ) );
     server.use( server.router );
 } );
 
-server.configure( 'development', function() {
-    server.use( express.errorHandler( { dumpExceptions: true, showStack: true } ) );
-} );
-
-server.configure( 'production', function() {
-    server.use( express.errorHandler() );
-} );
-
-
-// Start server - hook in sockets instance
-exports.io = io.listen( http.createServer( server ).listen( server.get( 'port' ), function() {
+// Start server 
+http.createServer( server ).listen( server.get( 'port' ), function() {
     console.log( 'Express server listening on ' + server.get( 'port' ) );
-} ) );
+} );
 
 // Configure Routes
 require( './routes' );
-
-// Configure Sockets
-require( './sockets');
-
-// Configure Database
-require( './db' );
